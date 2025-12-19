3eb2f278d9b719bebe93d1dd5fe98e9e736534db
Aman Kagajwala <amanktc59@gmail.com>
2025-12-19 11:32:57 +0530

    Terraform AWS EC2 with Nginx setup

---
+++ instance.tf
@@ -0,0 +1,34 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "nginx_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = "nginx-terraform-instance"
+  }
+}
